db49b6ca2c375d36f2317297627d3a882ff5855c
Lars Jelleryd <larsj99@yahoo.se>
2019-05-07 23:23:58 +0200

    Get search results via the backend (server app)

--- client/src/App.js
+++ client/src/App.js
@@ -16,16 +16,17 @@ class App extends Component {
     this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
   }
 
+  /*
+  Sends a request to backend with POST-method.
+   */
   getPlaces = async (fromPlace, toPlace) => {
-    let apiKey = process.env.REACT_APP_APIKEY
-    let response = await fetch(`http://free.rome2rio.com/api/1.4/json/Search?key=${apiKey}&oName=${fromPlace}&dName=${toPlace}&noRideshare`, {
+    let response = await fetch('api/getSearchResults/', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({ fromPlace: fromPlace, toPlace: toPlace })
     })
-    
     let data = await response.json()
     return data
   }
@@ -50,20 +51,20 @@ class App extends Component {
   render() {
     const places = this.state.places.length
       ? this.state.places.map((place, i) => {
-          return (
-            <li key={`${i}-react-key`}>
-              <h3>{place.name}</h3>
-              <p>{place.kind}</p>
-              <p>
-                {place.lat}, {place.lng}
-              </p>
-              <p>{place.longName}</p>
-            </li>
-          )
-        })
+        return (
+          <li key={`${i}-react-key`}>
+            <h3>{place.name}</h3>
+            <p>{place.kind}</p>
+            <p>
+              {place.lat}, {place.lng}
+            </p>
+            <p>{place.longName}</p>
+          </li>
+        )
+      })
       : null
 
-      
+
 
 
     const data = this.state.places;
@@ -71,7 +72,7 @@ class App extends Component {
       <div className='App'>
         <h3>Enter your prerequisites! </h3>
         <form className="formLayout" onSubmit={this.handleSearchSubmit}>
-          
+
           <input
             onChange={e => this.setState({ searchValue1: e.target.value })}
             className="input"
@@ -87,37 +88,37 @@ class App extends Component {
             id="destination"
             name='destination'
             placeholder='Choose destination...'
-            required="required"   
+            required="required"
           />
           <div className="inputFields">
-          <label className="label" htmlFor="fromDate">Departure date</label> 
-          <input
-          onChange={e => this.setState({ fromDate: e.target.value })} 
-          className="input"
-          id="fromDate"
-          type="date" 
-          max="2025-01-01" min="2024-01-01" 
-          required="required">
-          </input>
-          <label className="label" htmlFor="toDate">Going home date</label>
-          <input
-          onChange={e => this.setState({ toDate: e.target.value })}
-          className="input"
-          id="toDate"
-          type="date" 
-          max="2025-01-01" min="2024-01-01"
-          required="required">
-          </input>
-          <button style={{ margin: '0 0 0 20px' }}>Sök</button>
+            <label className="label" htmlFor="fromDate">Departure date</label>
+            <input
+              onChange={e => this.setState({ fromDate: e.target.value })}
+              className="input"
+              id="fromDate"
+              type="date"
+              max="2025-01-01" min="2024-01-01"
+              required="required">
+            </input>
+            <label className="label" htmlFor="toDate">Going home date</label>
+            <input
+              onChange={e => this.setState({ toDate: e.target.value })}
+              className="input"
+              id="toDate"
+              type="date"
+              max="2025-01-01" min="2024-01-01"
+              required="required">
+            </input>
+            <button style={{ margin: '0 0 0 20px' }}>Sök</button>
           </div>
         </form>
         <datalist id="destinations">
-        <option>Stockholm</option>
-        <option>Falun</option> 
-        <option>Åre</option>   
+          <option>Stockholm</option>
+          <option>Falun</option>
+          <option>Åre</option>
         </datalist>
         <div>
-          <Tables places={data.places}/>
+          <Tables places={data.places} />
           <h2>Results</h2>
           <ul>{places}</ul>
         </div>
