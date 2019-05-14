08e66558afa317a0b7846a6db75162f66cac1ac1
ChrilleJigling <Christianjigling@hotmail.com>
2019-05-14 18:33:38 +0200

    Commit #2, added the color-codes & did some work on the Front-END, SpeciallyHeader & Footer.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./Footer.css";
+import logo from './projektlogga.png'
+import twitter from './sweTwitter.jpg'
 
 class Footer extends React.Component {
     render() {
         return(
-            <footer className="main-footer">The footer</footer>
+            <footer className="main-footer">
+            <img className="logo2" src={logo} alt="Logo"></img>
+            <img className="twitter" src={twitter} alt="TwitterLogo"></img>
+            <p className="footerText">
+            Christian Jigling | Lars Jelleryd | Stefan Ohlsson | Ida Maria Sollo | Ahmed
+            </p>
+            </footer>
         )
     }
 }
