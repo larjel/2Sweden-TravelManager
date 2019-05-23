4628af9675595a9999bc1fad3e31ae5304bb2230
Lars Jelleryd <larsj99@yahoo.se>
2019-05-23 13:31:16 +0200

    Dummy hrefs added to avoid compiler warning

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,19 +5,19 @@ import "./Header.css"
 
 class Header extends React.Component {
     render() {
-        return(
+        return (
             <header className="main-head">
 
-                <img className="logo" src={logo} alt="Logo"/>
+                <img className="logo" src={logo} alt="Logo" />
                 <h1 className="headerText">
-                WINTER OLYMPIC GAMES 2024
+                    WINTER OLYMPIC GAMES 2024
                 </h1>
                 <nav className="nav">
                     <ul className="ul">
-                        <li className="current li" ><a href="">New trip</a></li>
-                        <li className="li"><a href="">About</a></li>
-                        <li className="li"><a href="">Locations</a></li>
-                        <li className="li"><a href="">Recommendations</a></li>
+                        <li className="current li" ><a href="/">New trip</a></li>
+                        <li className="li"><a href="/">About</a></li>
+                        <li className="li"><a href="/">Locations</a></li>
+                        <li className="li"><a href="/">Recommendations</a></li>
                     </ul>
                 </nav>
             </header>
