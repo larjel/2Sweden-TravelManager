17c9ed741edd1595e6f52a632f5a9d7bb40d62ad
ChrilleJigling <Christianjigling@hotmail.com>
2019-05-31 10:41:11 +0200

    Added hamburgermenu for Screens 1199px and less

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,9 +3,9 @@ import logo from './projektlogga.png'
 import twitter from './sweTwitter.jpg'
 import "./Header.css"
 import { GoSearch, GoInfo, GoLocation, GoHome, GoThumbsup } from 'react-icons/go';
-/*const style = {
-    marginTop: "20px"    
-}*/
+import Hamburgermenu from "./Hamburgermenu"
+import Media from 'react-media';
+
 class Header extends React.Component {
 
     handleClick = (e, val) => {
@@ -22,16 +22,28 @@ class Header extends React.Component {
                 <h1 className="headerText">
                     WINTER OLYMPIC GAMES 2024
                 </h1>
-                <nav className="myNavbar">
-                    <ul className="ul">
-                        <li className="current li" ><a className="active" href="/" onClick={(e) => this.handleClick(e, 'home')}><GoHome /> Home</a></li>
-                        <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'search')}><GoSearch /*style={style} */ /> New trip</a></li>
-                        <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'info')}><GoInfo /> Information</a></li>
-                        <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'locations')}><GoLocation /> Locations</a></li>
-                        <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'recommend')}><GoThumbsup /> Recommendations</a></li>
-                    </ul>
+                <Media query="(max-width: 1199px)">
+                {matches =>
+                matches ? (
+                    <Hamburgermenu/>
+                    ) : (
+                    <nav className="myNavbar">
+                        <ul className="ul">
+                            <li className="current li" ><a className="active" href="/" onClick={(e) => this.handleClick(e, 'home')}><GoHome /> Home</a></li>
+                            <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'search')}><GoSearch /*style={style} */ /> New trip</a></li>
+                            <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'info')}><GoInfo /> Information</a></li>
+                            <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'locations')}><GoLocation /> Locations</a></li>
+                            <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'recommend')}><GoThumbsup /> Recommendations</a></li>
+                        </ul>
+                    </nav> 
+                    )
+                    }
+                </Media>
 
-                </nav>
+
+
+
+                    
             </header>
         )
     }
