c8433ea8e45f4c382e7b1f756e333d2a5a06fad0
Lars Jelleryd <larsj99@yahoo.se>
2019-05-31 12:41:16 +0200

    Dropdown menu for destination information (Stockholm, Falun, Åre)

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,45 +8,68 @@ import Media from 'react-media';
 
 class Header extends React.Component {
 
-    handleClick = (e, val) => {
-        e.preventDefault();
-        console.log('The link was clicked. Value: ' + val);
-        this.props.setActiveMainPage(val);
+  constructor() {
+    super();
+
+    this.state = {
+      displayMenu: false,
     };
 
-    render() {
-        return (
-            <header className="main-head">
+    this.showDropdownMenu = this.showDropdownMenu.bind(this);
+    this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  };
 
-                <img className="logo" src={logo} alt="Logo" />
-                <h1 className="headerText">
-                    WINTER OLYMPIC GAMES 2024
+  showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+      document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  handleClick(e, val) {
+    e.preventDefault();
+    console.log('The link was clicked. Value: ' + val);
+    this.props.setActiveMainPage(val);
+  }
+
+  render() {
+    return (
+      <header className="main-head">
+
+        <img className="logo" src={logo} alt="Logo" />
+        <h1 className="headerText">
+          WINTER OLYMPIC GAMES 2024
                 </h1>
-                <Media query="(max-width: 1199px)">
-                {matches =>
-                matches ? (
-                    <Hamburgermenu/>
-                    ) : (
-                    <nav className="myNavbar">
-                        <ul className="ul">
-                            <li className="current li" ><a className="active" href="/" onClick={(e) => this.handleClick(e, 'home')}><GoHome /> Home</a></li>
-                            <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'search')}><GoSearch /*style={style} */ /> New trip</a></li>
-                            <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'info')}><GoInfo /> Information</a></li>
-                            <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'locations')}><GoLocation /> Locations</a></li>
-                            <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'recommend')}><GoThumbsup /> Recommendations</a></li>
-                        </ul>
-                    </nav> 
-                    )
-                    }
-                </Media>
+        <Media query="(max-width: 1199px)">
+          {matches =>
+            matches ? (
+              <Hamburgermenu />
+            ) : (
+                <nav className="myNavbar">
+                  <ul className="ul">
+                    <li className="current li" ><a className="active" href="/" onClick={(e) => this.handleClick(e, 'home')}><GoHome /> Home</a></li>
+                    <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'search')}><GoSearch /*style={style} */ /> New trip</a></li>
+                    <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'info')}><GoInfo /> Information</a></li>
+                    <li className="li" >
+                      <a href="/" onClick={this.showDropdownMenu}><GoLocation /> Locations</a>
+                    </li>
+                    <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'recommend')}><GoThumbsup /> Recommendations</a></li>
+                  </ul>
+                </nav>
+              )
+          }
+        </Media>
 
-
-
-
-                    
-            </header>
-        )
-    }
+      </header>
+    )
+  }
 }
 
 export default Header;
