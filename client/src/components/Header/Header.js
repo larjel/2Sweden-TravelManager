58be2306102d3a8dd81ed8f57f4b0fb149d12a06
Lars Jelleryd <larsj99@yahoo.se>
2019-06-04 18:21:27 +0200

    Drop down menu

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from './projektlogga.png'
 import twitter from './sweTwitter.jpg'
 import "./Header.css"
+import "./dropdown.css"
 import { GoSearch, GoInfo, GoLocation, GoHome, GoThumbsup } from 'react-icons/go';
 import Hamburgermenu from "./Hamburgermenu"
 import Media from 'react-media';
@@ -10,28 +11,10 @@ class Header extends React.Component {
 
   constructor() {
     super();
-
     this.state = {
-      displayMenu: false,
     };
-
-    this.showDropdownMenu = this.showDropdownMenu.bind(this);
-    this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
     this.handleClick = this.handleClick.bind(this);
   };
-
-  showDropdownMenu(event) {
-    event.preventDefault();
-    this.setState({ displayMenu: true }, () => {
-      document.addEventListener('click', this.hideDropdownMenu);
-    });
-  }
-
-  hideDropdownMenu() {
-    this.setState({ displayMenu: false }, () => {
-      document.removeEventListener('click', this.hideDropdownMenu);
-    });
-  }
 
   handleClick(e, val) {
     e.preventDefault();
@@ -46,28 +29,24 @@ class Header extends React.Component {
         <img className="logo" src={logo} alt="Logo" />
         <h1 className="headerText">
           WINTER OLYMPIC GAMES 2024
-                </h1>
+        </h1>
         <Media query="(max-width: 1199px)">
           {matches =>
             matches ? (
               <Hamburgermenu />
             ) : (
-                <nav className="myNavbar">
+                <nav className="my-navbar">
                   <ul className="ul">
                     <li className="current li" ><a className="active" href="/" onClick={(e) => this.handleClick(e, 'home')}><GoHome /> Home</a></li>
-                    <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'search')}><GoSearch /*style={style} */ /> New trip</a></li>
+                    <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'search')}><GoSearch /> New search</a></li>
                     <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'info')}><GoInfo /> Information</a></li>
-                    <li className="li" >
-                      <a href="/" onClick={this.showDropdownMenu}><GoLocation /> Locations</a>
-                      {this.state.displayMenu ? (
-                        <ul>
-                          <li><a className="active" href="/" onClick={(e) => this.handleClick(e, 'stockholm')}>Stockholm</a></li>
-                          <li><a href="/" onClick={(e) => this.handleClick(e, 'falun')}>Falun</a></li>
-                          <li><a href="/" onClick={(e) => this.handleClick(e, 'are')}>Åre</a></li>
-                        </ul>
-                      ) :
-                        (null)
-                      }
+                    <li className="li dropdown" >
+                      <button className="dropbtn"><GoLocation /> Locations</button>
+                      <div className="dropdown-content">
+                        <a href="/" onClick={(e) => this.handleClick(e, 'stockholm')}>Stockholm</a>
+                        <a href="/" onClick={(e) => this.handleClick(e, 'falun')}>Falun</a>
+                        <a href="/" onClick={(e) => this.handleClick(e, 'are')}>Åre</a>
+                      </div>                               
                     </li>
                     <li className="li" ><a href="/" onClick={(e) => this.handleClick(e, 'recommend')}><GoThumbsup /> Recommendations</a></li>
                   </ul>
