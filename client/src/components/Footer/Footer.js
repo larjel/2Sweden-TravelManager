ed8ccc1c0e10cee7ef27e1d3186912a8cef538be
Lars J <larsj99@yahoo.se>
2019-07-17 21:39:22 +0200

    Feedback fix: Prefer to use of functional component when no state
    Prefer to use of functional components when a component only return jsx-elements and have no need for state.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,21 +1,18 @@
-import React from 'react'
+import React from 'react';
 import "./Footer.css";
 import logo from './projektlogga.png'
 import twitter from './sweTwitter.jpg'
 
-class Footer extends React.Component {
-    render() {
-        return (
-            <footer className="main-footer">
-                <img className="logo2" src={logo} alt="Logo"></img>
-                <img className="twitter" src={twitter} alt="TwitterLogo"></img>
-                <p className="footerText">
-                    Christian Jigling | Lars Jelleryd | Stefan Ohlsson | Ida Maria Solli | Ahmed Ismail
-                </p>
-            </footer>
-        )
-    }
+const Footer = () => {
+    return (
+        <footer className="main-footer">
+            <img className="logo2" src={logo} alt="Logo"></img>
+            <img className="twitter" src={twitter} alt="TwitterLogo"></img>
+            <p className="footerText">
+                Christian Jigling | Lars Jelleryd | Stefan Ohlsson | Ida Maria Solli | Ahmed Ismail
+            </p>
+        </footer>
+    )
 }
-
 
 export default Footer;
